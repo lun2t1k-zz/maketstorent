0f187f40ae2828343f3c25aa8f9eeb3b031987f9
Dmitry Krutkin <84284545+lun2t1k@users.noreply.github.com>
2021-07-13 17:59:39 +0600

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.nav');
+const menu = document.querySelector('.navbar');
 const closeMenuButton = document.querySelector('.close-button');
 
 menuButton.addEventListener('click', function () {
